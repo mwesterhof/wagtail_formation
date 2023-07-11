3673b63569acd656c27e6fae605815d8a3ed7ec4
Marco Westerhof <m.westerhof@lukkien.com>
2023-07-11 11:15:29 +0200

    fixing long_description

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     description="Build forms from wagtail blocks",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='wagtail_formation',
     name='wagtail_formation',
